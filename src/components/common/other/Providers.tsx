bd946395cebc840981dd13911c86f513e38b05b9
Lars Kvihaugen <lars@kvihaugen.no>
2026-03-03 14:00:03 +0100

    [main]: Installed Mantine UI form and notification extensions

--- src/components/common/other/Providers.tsx
+++ src/components/common/other/Providers.tsx
@@ -3,6 +3,7 @@
 import ParentProps from "@/types/common/ParentProps";
 
 import { createTheme, MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 import "@/utils/i18n";
 
@@ -10,6 +11,7 @@ const theme = createTheme({});
 
 const Providers = ({ children }: ParentProps) => (
     <MantineProvider theme={theme} defaultColorScheme="auto">
+        <Notifications />
         {children}
     </MantineProvider>
 );
